52ce8ec05d1b54a1d2e7f3344a7fb11b3a1e628a
CaroNew <125692041+CaroNew@users.noreply.github.com>
2023-03-14 12:41:11 -0300

    desafio tres

---
+++ challenge-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a = 0;
+    int b = 0;
+    float c = 0;
+    float d = 0;
+    scanf("%d %d", &a, &b);
+    scanf("%f %f", &c, &d);
+    printf("%d %d\n", a+b, a-b);
+    printf("%.1f %.1f\n", c+d, c-d);
+    
+    
+    return 0;
+}
+
